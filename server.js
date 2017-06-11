08394b1088ad4aa217cd82c080d85ca53b731cbb
Michael Pimentel <mtpjr88@gmail.com>
2017-06-10 18:14:15 -0700

    add put todos

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	let body = _.pick(req.body, 'description', 'completed'); // use_.pick to only pick description and completed
 
-	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0) {
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 		return res.status(400).send();
 	}
 
@@ -62,7 +62,33 @@ app.delete('/todos/:id', function(req, res) {
 	}
 });
 
+// PUT .todos/:id
+app.put('/todos/:id', function (req,res) {
+	const todoId = parseInt(req.params.id, 10);
+	const matchedTodo = _.findWhere(todos, {id: todoId});
+	const body = _.pick(req.body, 'description', 'completed');
+	let validAttributes = {};
 
+	if (!matchedTodo){
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+			return res.status(400).send();
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	}	else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+		_.extend(matchedTodo, validAttributes);
+		res.json(matchedTodo);
+
+});
 app.listen(PORT, function() {
 	console.log('Express listening on port ' + PORT);
 });
